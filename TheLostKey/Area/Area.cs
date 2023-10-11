c44ed2c2979db003b217ef60881bbca71756b2bb
mezmeri <madsschmidt33@gmail.com>
2023-10-11 13:49:10 +0200

    Added a method that creates a table which represents the tiles of a room.

---
+++ TheLostKey/Area/Area.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Spectre.Console;
+using Spectre.Console.Rendering;
+
+namespace TheLostKey.Area
+{
+    public class Area
+    {
+        string name;
+        string description;
+        public string Name => name;
+
+        public string Description => description;
+
+        public Area(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        public void BuildArea(int row, int column)
+        {
+            var table = new Table();
+            for (int i = 0; i < column; i++)
+            {
+                table.AddColumn(new TableColumn(" "));
+            }
+
+            for (int i = 0; i < row; i++)
+            {
+                table.AddEmptyRow();
+            }
+
+            table.HideHeaders();
+            table.Width = 100;
+
+
+
+            AnsiConsole.Write(table);
+        }
+    }
+}
---
+++ TheLostKey/Area/Castle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLostKey.Area
+{
+    internal class Castle : Area
+    {
+        public Castle(string name, string description) : base(name, description)
+        {
+
+        }
+
+        
+    }
+}
