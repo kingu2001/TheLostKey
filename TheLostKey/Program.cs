4a4579bc016b802961bc62b9fc29422caa7b57cf
TheAltintas <altintas12@outlook.dk>
2023-10-11 12:11:07 +0200

    Første commit

--- TheLostKey/Program.cs
+++ TheLostKey/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to TheLostKey");
+
+            Console.ReadLine();
         }
     }
 }
