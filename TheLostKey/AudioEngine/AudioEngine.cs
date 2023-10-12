1ba77c3a9ee3756c1b8ad905b139856e86628552
Rasmus Olsen <rasmus782@gmail.com>
2023-10-12 08:48:07 +0200

    Made an audioEngine to play sound fx

---
+++ TheLostKey/AudioEngine/AudioEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace TheLostKey.AudioEngine
+{
+    internal class AudioEngine
+    {
+        public static void PlaySound(string fileName)
+        {
+            using (var output = new WaveOutEvent())
+            using (var player = new AudioFileReader(fileName))
+            {
+                output.Init(player);
+                output.Play();
+                while(output.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(500);
+                }
+            }
+
+        }
+    }
+}
